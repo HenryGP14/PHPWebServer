8977c9af16185dca724d644fbe66514c923e1106
Henry Pérez <henry.perez2017@uteq.edu.ec>
2021-07-19 17:38:26 -0500

    Cambio de web server

--- index.php
+++ index.php
@@ -1,29 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+include('./settings/config.php');
+include('./settings/utils.php');
+$object = new Conection();
+$conection = $object->connect($db);
+$query = "Select * from mensajes";
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+$resultado = $conection->prepare($query);
+$resultado->execute();
 
-<body>
-    <div>
-        <?php
-        include('./settings/config.php');
-        include('./settings/utils.php');
-        $object = new Conection();
-        $conection = $object->connect($db);
-        $query = "Select * from mensajes";
-
-        $resultado = $conection->prepare($query);
-        $resultado->execute();
-        $datos = $resultado->fetchAll();
-
-        var_dump($datos);
-        ?>
-    </div>
-</body>
-
-</html>
+header("HTTP/1.1 200 OK");
+echo json_encode($resultado->fetchAll());
+exit();
--- settings/utils.php
+++ settings/utils.php
@@ -4,7 +4,13 @@ class Conection
     function connect($db)
     {
         try {
-            $pgcon = new PDO("pgsql:host={$db['host']} dbname={$db['database']} user={$db['username']} password={$db['password']}");
+            $pgcon = new PDO(
+                "pgsql:host={$db['host']}; dbname={$db['database']}; port=5432; ",
+                $db['username'],
+                $db['password'],
+                array(PDO::ATTR_PERSISTENT => true)
+            );
+            $pgcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pgcon;
         } catch (Exception $e) {
             die("El error de conexión es: " . $e->getMessage());
