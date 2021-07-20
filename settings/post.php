193987b6a0f7b0ccb5976a43df12c32cc28a1be3
Carlos10Almeida <carlos.almeida2017@uteq.edu.ec>
2021-07-19 22:33:29 -0500

    Arreglo del Web Service

--- settings/post.php
+++ settings/post.php
@@ -7,73 +7,36 @@ $conection = $object->connect($db);
 
 // Método GET
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if ($_GET['consulta'] == 'provincias') {
+    $cant_variables = count($_GET);
+    // Recibir un dato si se envia un id
+    if ($cant_variables > 0) {
         $key = array_keys($_GET);
-        if ($key[1] == 'id') {
-            $query = "SELECT * FROM provincia WHERE idprovincia=:id";
-            $result = $conection->prepare($query);
-            $result->bindValue(':id', $_GET['id']);
-            $result->execute();
-
-            echo (array_keys($_GET)[1]);
-
-            header("HTTP/1.1 200 OK");
-            echo json_encode($result->fetch(PDO::FETCH_ASSOC));
-            exit();
-        } else if ($key[1] == 'nom-provincia') {
-            $valor = str_replace('"', "'", $_GET['nom-provincia']);
-            $query = "SELECT * FROM provincia WHERE nombre=" . $valor;
+        if ($key[0] == 'id-provincia') {
+            $query = "SELECT * FROM canton WHERE idprovincia='{$_GET['id-provincia']}'";
             $result = $conection->prepare($query);
             $result->execute();
-
-            header("HTTP/1.1 200 OK");
-            echo json_encode($result->fetch(PDO::FETCH_ASSOC));
-            exit();
-        } else {
-            $query = "SELECT * FROM provincia";
-            $result = $conection->prepare($query);
-            $result->execute();
-            $result->setFetchMode(PDO::FETCH_ASSOC);
 
             header("HTTP/1.1 200 OK");
             echo json_encode($result->fetchAll());
             exit();
-        }
-
-        // Cantones
-    } else if ($_GET['consulta'] == 'cantones') {
-        $key = array_keys($_GET);
-        if ($key[1] == 'id') {
-            $query = "SELECT * FROM provincia WHERE idprovincia=:id";
-            $result = $conection->prepare($query);
-            $result->bindValue(':id', $_GET['id']);
-            $result->execute();
-
-            echo (array_keys($_GET)[1]);
-
-            header("HTTP/1.1 200 OK");
-            echo json_encode($result->fetch(PDO::FETCH_ASSOC));
-            exit();
-        } else if ($key[1] == 'nom-provincia') {
+        } else if ($key[0] == 'nom-provincia') {
             $valor = str_replace('"', "'", $_GET['nom-provincia']);
             $query = "SELECT * FROM provincia WHERE nombre=" . $valor;
             $result = $conection->prepare($query);
             $result->execute();
-
-            header("HTTP/1.1 200 OK");
-            echo json_encode($result->fetch(PDO::FETCH_ASSOC));
-            exit();
-        } else {
-            $query = "SELECT * FROM canton";
-            $result = $conection->prepare($query);
-            $result->execute();
-            $result->setFetchMode(PDO::FETCH_ASSOC);
 
             header("HTTP/1.1 200 OK");
             echo json_encode($result->fetchAll());
             exit();
         }
     } else {
-        echo ("Página no encontrada");
+        $query = "SELECT * FROM provincia";
+        $result = $conection->prepare($query);
+        $result->execute();
+        $result->setFetchMode(PDO::FETCH_ASSOC);
+
+        header("HTTP/1.1 200 OK");
+        echo json_encode($result->fetchAll());
+        exit();
     }
 }
